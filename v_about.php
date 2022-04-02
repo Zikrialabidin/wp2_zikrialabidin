04e9423b1ef478b30298554772579cefaf8326b1
Zikrialabidin <101872245+Zikrialabidin@users.noreply.github.com>
2022-04-02 21:11:35 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,30 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan :Zikrial</li>
+        <li>Nama Belakang : Abidin</li>
+    </ul>
+    <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+        <li> Jalan Cikande Permai</li>
+    </ul>
+ 
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+        <li>Serang</li>
+    </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+        <li>Badmintoon</li>
+        <li>Futsal</li>
+    </ul>
+</section>
+
+
+
+
+
+
